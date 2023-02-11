0fe957342e1a7b401672048e2d39cdea47ab0001
Punvireakroth <vireakrothpun@gmail.com>
2023-02-11 19:55:48 +0700

    add some form

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,25 @@ export default function Login() {
         <title>Login</title>
       </Head>
 
-      <h1>Login</h1>
+      <section className="w-3/4 mx-auto flex flex-col gap-10">
+        <div className="title">
+          <h1 className="text-gray-900 text-4xl font-bold py-4">Login here</h1>
+          <p className="w-3/4 mx-auto text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            laboriosam ducimus porro perspiciatis sit. Dolores perferendis fugit
+            doloremque vitae similique, est itaque perspiciatis eius eaque
+            veritatis asperiores animi non quibusdam.
+          </p>
+        </div>
+
+        {/* form */}
+        <form className="flex flex-col gap-5 mx-auto">
+          <div className="input-group">
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+        </form>
+        {/* bottom */}
+      </section>
     </Layout>
   );
 }
