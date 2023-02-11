00efe21030cdf9e319742eb2585b6f819e9852ab
Punvireakroth <vireakrothpun@gmail.com>
2023-02-11 20:17:52 +0700

    styled inputs

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Layout from "../layout/Auth";
+import Link from "next/link";
+import styles from "../styles/Form.module.css";
 export default function Login() {
   return (
     <Layout>
@@ -11,20 +13,49 @@ export default function Login() {
         <div className="title">
           <h1 className="text-gray-900 text-4xl font-bold py-4">Login here</h1>
           <p className="w-3/4 mx-auto text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-            laboriosam ducimus porro perspiciatis sit. Dolores perferendis fugit
-            doloremque vitae similique, est itaque perspiciatis eius eaque
-            veritatis asperiores animi non quibusdam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
         </div>
 
         {/* form */}
         <form className="flex flex-col gap-5 mx-auto">
-          <div className="input-group">
-            <input type="email" name="email" placeholder="Email" />
+          <div className={styles.input_group}>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className={styles.input_text}
+            />
+          </div>
+          <div className={styles.input_group}>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className={styles.input_text}
+            />
+          </div>
+        </form>
+        <form className="flex flex-col gap-5 mx-auto">
+          {/* login button */}
+          <div className="input-button text-gray-500">
+            <button type="submit">Login</button>
+          </div>
+          <div className="input-button text-gray-500">
+            <button type="submit">Sign In with Google</button>
+          </div>
+          <div className="input-button text-gray-500">
+            <button type="submit">Sign In with Github</button>
           </div>
         </form>
         {/* bottom */}
+        <p className="text-center text-gray-500">
+          Don't have account ?{" "}
+          <Link href={"/register"}>
+            {" "}
+            <a className="text-blue-500">Click here</a>{" "}
+          </Link>
+        </p>
       </section>
     </Layout>
   );
