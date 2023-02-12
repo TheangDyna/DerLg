d7f30db075e218c41a5ed7ac36811bbea17d74d5
Punvireakroth <vireakrothpun@gmail.com>
2023-02-12 11:49:21 +0700

    added some login icons

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,10 @@ import Head from "next/head";
 import Layout from "../layout/Auth";
 import Link from "next/link";
 import styles from "../styles/Form.module.css";
+import Image from "next/image";
+import { HiOutlineMail } from "react-icons/hi";
+import { MdPassword } from "react-icons/md";
+
 export default function Login() {
   return (
     <Layout>
@@ -9,9 +13,9 @@ export default function Login() {
         <title>Login</title>
       </Head>
 
-      <section className="w-3/4 mx-auto flex flex-col gap-10">
+      <section className="w-3/4 mx-auto flex flex-col gap-10 ">
         <div className="title">
-          <h1 className="text-gray-900 text-4xl font-bold py-4">Login here</h1>
+          <h1 className="text-gray-900 text-4xl font-bold py-4">Login</h1>
           <p className="w-3/4 mx-auto text-gray-500">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
@@ -26,6 +30,9 @@ export default function Login() {
               placeholder="Email"
               className={styles.input_text}
             />
+            <span className="icon flex items-center px-4">
+              <HiOutlineMail size={25}></HiOutlineMail>
+            </span>
           </div>
           <div className={styles.input_group}>
             <input
@@ -34,26 +41,37 @@ export default function Login() {
               placeholder="Password"
               className={styles.input_text}
             />
+            <span className="icon flex items-center px-4">
+              <MdPassword size={25}></MdPassword>
+            </span>
           </div>
         </form>
         <form className="flex flex-col gap-5 mx-auto">
           {/* login button */}
           <div className="input-button text-gray-500">
-            <button type="submit">Login</button>
+            <button type="submit" className={styles.button}>
+              Login
+            </button>
           </div>
           <div className="input-button text-gray-500">
-            <button type="submit">Sign In with Google</button>
+            <button type="button" className={styles.button_costum}>
+              <Image src={"/asset/google.svg"} width={20} height={20}></Image>{" "}
+              Sign In with Google
+            </button>
           </div>
           <div className="input-button text-gray-500">
-            <button type="submit">Sign In with Github</button>
+            <button type="button" className={styles.button_costum}>
+              <Image src={"/asset/github.svg"} width={25} height={25}></Image>{" "}
+              Sign In with Github
+            </button>
           </div>
         </form>
         {/* bottom */}
         <p className="text-center text-gray-500">
-          Don't have account ?{" "}
-          <Link href={"/register"}>
-            {" "}
-            <a className="text-blue-500">Click here</a>{" "}
+          Don't have account ?
+          <Link href={"/register"} className="text-blue-500">
+            {/* <a className="text-blue-500">Click here</a> */}
+            Click here
           </Link>
         </p>
       </section>
