0be3271abed247390ce5aa2fc7042def1c255ce3
Punvireakroth <vireakrothpun@gmail.com>
2023-02-12 11:48:48 +0700

    modify some layout

--- layout/Auth.js
+++ layout/Auth.js
@@ -1,9 +1,15 @@
+import style from "../styles/Auth.module.css";
+
 export default function Auth({ children }) {
   return (
     <div className="flex h-screen">
-      <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
-        <div></div>
-        <div className="right flex flex-col justify-evenly bg-grey-500">
+      <div className="m-auto bg-slate-50 rounded-md w-3/7 h-3/4">
+        {/* <div className={style.imgStyle}>
+           <div className="flex items-center mx-auto">
+            <h1>DerLg</h1>
+          </div> 
+        </div> */}
+        <div className="right flex flex-col justify-evenly">
           <div className="text-center py-10">{children}</div>
         </div>
       </div>
