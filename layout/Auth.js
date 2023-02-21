d2428961bb4556d2175eca52f94ae670ef063074
Punvireakroth <vireakrothpun@gmail.com>
2023-02-21 21:28:42 +0700

    Login Layout

--- layout/Auth.js
+++ layout/Auth.js
@@ -2,7 +2,7 @@ import style from "../styles/Auth.module.css";
 
 export default function Auth({ children }) {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen ">
       <div className="m-auto bg-slate-50 rounded-md w-3/7 h-3/4">
         {/* <div className={style.imgStyle}>
            <div className="flex items-center mx-auto">
