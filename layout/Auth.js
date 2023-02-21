2ec83ab215df3b306acbec30d0cb0b1ec8f582c3
Punvireakroth <vireakrothpun@gmail.com>
2023-02-21 22:51:40 +0700

    handle the height

--- layout/Auth.js
+++ layout/Auth.js
@@ -3,7 +3,7 @@ import style from "../styles/Auth.module.css";
 export default function Auth({ children }) {
   return (
     <div className="flex h-screen">
-      <div className="m-auto bg-slate-50 rounded-md  md:w-1/3 sm:w-3/6 lg:w-1/4 h-3/7">
+      <div className="m-auto bg-slate-50 rounded-md md:w-2/5 sm:w-3/6 lg:w-1/4 h-3/7">
         {/* <div className={style.imgStyle}>
            <div className="flex items-center mx-auto">
             <h1>DerLg</h1>
