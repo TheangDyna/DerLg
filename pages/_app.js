e6854ad317d5afb864faadd3e39b507a5cfff713
Punvireakroth <vireakrothpun@gmail.com>
2023-02-22 23:20:25 +0700

    make the app share the same session

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
+// https://next-auth.js.org/getting-started/example
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />;
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
