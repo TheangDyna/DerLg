b03b10908c47ddeee5d0a9665150d77fa502b9c5
Punvireakroth <vireakrothpun@gmail.com>
2023-02-23 00:04:52 +0700

    added background picture

--- layout/Auth.js
+++ layout/Auth.js
@@ -1,14 +1,8 @@
 import style from "../styles/Auth.module.css";
-
 export default function Auth({ children }) {
   return (
-    <div className="flex h-screen">
+    <div className={`flex h-screen ${style.bgImage}`}>
       <div className="m-auto bg-slate-50 rounded-md md:w-2/5 sm:w-3/6 lg:w-1/4 h-3/7">
-        {/* <div className={style.imgStyle}>
-           <div className="flex items-center mx-auto">
-            <h1>DerLg</h1>
-          </div> 
-        </div> */}
         <div className="right flex flex-col justify-evenly">
           <div className="text-center py-10">{children}</div>
         </div>
