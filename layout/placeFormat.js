88d70ad56d6f1dbb04ed1166177acffd7120492b
Punvireakroth <vireakrothpun@gmail.com>
2023-02-28 23:39:37 +0700

    added Format layout

--- layout/placeFormat.js
+++ layout/placeFormat.js
@@ -1,11 +1,15 @@
 import Header from "../components/places/header";
 import Footer from "../components/places/footer";
+import Head from "next/head";
 
-export default function placeFormat() {
+export default function placeFormat({ children }) {
   return (
     <div>
+      <Head>
+        <title>Provinces</title>
+      </Head>
       <Header></Header>
-      <main>Main Contents</main>
+      <main>{children}</main>
       <Footer></Footer>
     </div>
   );
