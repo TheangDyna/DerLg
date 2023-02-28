b0f50a4c3f67c79b7e98dac354c8da38849b5b4f
Punvireakroth <vireakrothpun@gmail.com>
2023-02-28 22:57:37 +0700

    create a layout component

---
+++ layout/placeFormat.js
@@ -0,0 +1,12 @@
+import Header from "../components/places/header";
+import Footer from "../components/places/footer";
+
+export default function placeFormat() {
+  return (
+    <div>
+      <Header></Header>
+      <main>Main Contents</main>
+      <Footer></Footer>
+    </div>
+  );
+}
