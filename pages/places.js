7e6fdb981f8e29b6b992e11e0199399ddda38f41
Punvireakroth <vireakrothpun@gmail.com>
2023-02-28 22:50:49 +0700

    new route

---
+++ pages/places.js
@@ -0,0 +1,11 @@
+import Header from "../components/places/header";
+import Footer from "../components/places/footer";
+
+export default function Places() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
