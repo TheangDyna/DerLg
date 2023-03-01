dffd44b96dcd3573a5de19905d387699c7d47c2d
theangdyna <theangdyna365@gmail.com>
2023-03-01 09:06:37 +0700

    edit test page

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,8 @@
-// https://www.w3schools.com/html/mov_bbb.mp4
-import HoverVideoPlayer from "react-hover-video-player";
+import React from "react";
 const Test = () => {
   return (
     <div>
-      <HoverVideoPlayer
-        videoSrc="https://www.w3schools.com/html/mov_bbb.mp4"
-      />
+      test page
     </div>
   );
 };
